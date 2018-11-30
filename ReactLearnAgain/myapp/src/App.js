3c326ac28dbe398264365d2c3600c0c186f936ee
study0918 <beyondthestars@yeah.net>
2018-11-30 22:33:34 +0800

    'react'

---
+++ ReactLearnAgain/myapp/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+import Header from './components/Header';
+import Home from './components/Home'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+          <div className="row">
+              <div className="col-xs-1 col-xs-offset-11">
+                  <Home/>
+              </div>
+          </div>
+          <div className="row">
+              <div className="col-xs-1 col-xs-offset-11">
+                  <h1>Hello!!</h1>
+              </div>
+          </div>
+          <div className="row">
+              <div className="col-xs-1 col-xs-offset-11">
+                  <Header/>
+              </div>
+          </div>
+      </div>
+    );
+  }
+}
+
+export default App;
