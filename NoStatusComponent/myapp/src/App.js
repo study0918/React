5a8287d643ed1bb08812341a3ed1ba7a884903fc
mysunshinehcy <1042006984@qq.com>
2018-12-02 18:37:34 +0800

    'comonpent'

--- NoStatusComponent/myapp/src/App.js
+++ NoStatusComponent/myapp/src/App.js
@@ -1,9 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component ,PureComponent} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//书写一个无状态组件
+class Temp extends PureComponent{
+  render(){
+    console.log("render Temp");
+  return (
+    <div>{this.props.val}</div>
+  );
+  }
+}
 class App extends Component {
+  state={
+    val:1
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        val: 1
+      })
+    }, 2000)
+  }
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log('nextState', nextState);
+  //   console.log('current state', this.state);
+  //   return (
+  //     this.state.val === nextState.val ? false : true
+  //   )
+  // }
   render() {
+    console.log("render App")
     return (
       <div className="App">
         <header className="App-header">
@@ -19,6 +48,7 @@ class App extends Component {
           >
             Learn React
           </a>
+          <Temp val={this.state.val}/>
         </header>
       </div>
     );
