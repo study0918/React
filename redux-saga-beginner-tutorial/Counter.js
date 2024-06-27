f55d873f690487e681999465507ddeac9ead09b4
李祥 <lixiang1@cmiot.chinamobile.com>
2024-06-27 12:50:40 +0800

    redux

--- redux-saga-beginner-tutorial/Counter.js
+++ redux-saga-beginner-tutorial/Counter.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { PropTypes } from "prop-types";
 
-const Counter = ({ value, onIncrement, onDecrement }) => (
+const Counter = ({ value, onIncrement, onDecrement, onIncrementAsync }) => (
   <div>
-    <button onClick={onIncrement}>Increment</button>{" "}
-    <button onClick={onDecrement}>Decrement</button>
-    <hr />
-    <div>Clicked: {value} times</div>
+    <button onClick={onIncrementAsync}>Increment after 1 second </button>{" "}
+    <button onClick={onIncrement}>Increment </button>{" "}
+    <button onClick={onDecrement}>Decrement </button> <hr />
+    <div>
+      Clicked: {value}
+      times
+    </div>{" "}
   </div>
 );
 
