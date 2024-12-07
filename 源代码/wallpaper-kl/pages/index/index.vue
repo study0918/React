2e2c4c083a0a63cce007c5851748d7d20276492b
study0918 <beyondthestars@yeah.net>
2024-12-07 21:43:26 +0800

    壁纸

--- 源代码/wallpaper-kl/pages/index/index.vue
+++ 源代码/wallpaper-kl/pages/index/index.vue
@@ -1,14 +1,42 @@
 <template>
 	<view class="homeLayout">
-		22
+		<view class="banner">
+			<swiper indicator-dots indicator-color="rgba(255,255,255,0.5)" indicator-active-color="#fff" autoplay>
+				<swiper-item v-for="item in 3">
+					<image src="../../common/images/banner1.jpg" mode="aspectFill"></image>
+				</swiper-item>
+			</swiper>
+		</view>
+
 	</view>
 </template>
 
 <script setup>
-	
+
 </script>
 
 <style lang="scss" scoped>
+	.homeLayout {
+		.banner {
+			width: 750rpx;
+			padding: 30rpx 0;
 
+			swiper {
+				width: 750rpx;
+				height: 340rpx;
 
+				&-item {
+					width: 750rpx;
+					height: 340rpx;
+					padding: 0 30rpx;
+
+					image {
+						width: 100%;
+						height: 100%;
+						border-radius: 10rpx;
+					}
+				}
+			}
+		}
+	}
 </style>
