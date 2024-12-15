fe1d445df56c9504b96f95f73b39b054789e786c
study0918 <beyondthestars@yeah.net>
2024-12-15 12:25:22 +0800

    graph

--- graph/src/components/graph.vue
+++ graph/src/components/graph.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="imageTableBox"></div>
+  <div id="main"></div>
 </template>
 
 <script>
@@ -23,49 +23,54 @@ echarts.use([
 ]);
 
 export default {
-  name: 'Advert-ising',
+  name: 'graph-echart',
   mounted() {
-    const chartDom = document.getElementById('imageTableBox');
-    const myChart = echarts.init(chartDom);
-    let option;
+    this.initChart();
+  },
+  methods: {
+    initChart() {
+      const chartDom = document.getElementById('main');
+      const myChart = echarts.init(chartDom);
+      let option;
 
-    option = {
-      title: {
-        text: 'Referer of a Website',
-        subtext: 'Fake Data',
-        left: 'center',
-      },
-      tooltip: {
-        trigger: 'item',
-      },
-      legend: {
-        orient: 'vertical',
-        left: 'left',
-      },
-      series: [
-        {
-          name: 'Access From',
-          type: 'pie',
-          radius: '50%',
-          data: [
-            // 根据业务替换成动态数据
-            { value: 1048, name: 'Search Engine' },
-            { value: 735, name: 'Direct' },
-            { value: 580, name: 'Email' },
-            { value: 484, name: 'Union Ads' },
-            { value: 300, name: 'Video Ads' },
-          ],
-          emphasis: {
-            itemStyle: {
-              shadowBlur: 10,
-              shadowOffsetX: 0,
-              shadowColor: 'rgba(0, 0, 0, 0.5)',
+      option = {
+        title: {
+          text: 'Referer of a Website',
+          subtext: 'Fake Data',
+          left: 'center',
+        },
+        tooltip: {
+          trigger: 'item',
+        },
+        legend: {
+          orient: 'vertical',
+          left: 'left',
+        },
+        series: [
+          {
+            name: 'Access From',
+            type: 'pie',
+            radius: '50%',
+            data: [
+              // 根据业务替换成动态数据
+              { value: 1048, name: 'Search Engine' },
+              { value: 735, name: 'Direct' },
+              { value: 580, name: 'Email' },
+              { value: 484, name: 'Union Ads' },
+              { value: 300, name: 'Video Ads' },
+            ],
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)',
+              },
             },
           },
-        },
-      ],
-    };
-    option && myChart.setOption(option);
+        ],
+      };
+      option && myChart.setOption(option);
+    },
   },
 };
 </script>
